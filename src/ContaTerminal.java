818ee82afd0b78289a2522aebefea1a9789629df
Renato Parra Silva <renato.parrasilva11@gmail.com>
2024-05-22 00:11:04 -0300

    Adicionando Funcionalidades

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.sql.SQLOutput;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu número da Conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite seu número da Agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite seu nome: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite seu saldo: ");
+        float saldo = scanner.nextFloat();
+
+        System.out.println("Olá "+ nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero +" e seu saldo de " + saldo+ " já está disponível para saque.");
     }
 }
